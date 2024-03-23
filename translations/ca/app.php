392883ac0cedd4ffe10c6693cf2cb80263857e4a
joel97vil <joel97vil@gmail.com>
2024-03-23 10:18:32 +0100

    Styles bugfix and translations

--- translations/ca/app.php
+++ translations/ca/app.php
@@ -8,4 +8,28 @@
     //Home part
     "Hello there, I'm" => "Hola a tothom, sóc en",
     "A FullStack Developer." => "Un desenvolupador d'aplicacions.",
+    "I'm a FullStack App and Web Developer, I love to create beautiful and functional solutions." => "Sóc un desenvolupador d'aplicacions Web i Escritori fullstack, m'encanta crear solcions boniques i funcionals.",
+    "Fullstack developer with more than four years of experience in the sector being part in various projects in a small development team." => "Desenvolupador fullstack amb més de quatre anys d'experiència en el sector, sent part in diferents projectes en un petit equip de desenvolupament.",
+    "Knowledgeable and collaborator in most project phases: analysis, functional requirements, documentations, task organization, development, testing, version control, steps to production, integrations and customer training." => "",
+    "Interested in cybersecurity, new development technologies and ensuring good user experience." => "M'agrada la ciberseguritat, les noves tecnologíes de desenvolupament, i garantitzar una bona experiència d'usuari.",
+
+    //About me part
+    'About' => "Sobre",
+    'me' => "mi",
+    'About me' => "Sobre mi",
+    'Information about me' => "Informació sobre mi",
+    "I've been developer since I was 11 years old, but I didn't knew that" => "He sigut desenvolupador desde que tenía 11 anys, però encara no ho sabía",
+    "I started programming at 11 because I was a big fan of RuneScape, a MMO-RPG. I wanted to investigate how the game could be built. And I starting my research of how to do it I found the RuneScape private servers world, which is a RS replica with Java Code..."
+        => "Vaig començar a programar als 11 perque era molt fan de RuneScape, un MMO-RPG. Volía investigar com estava construït aquell joc. Vaig començar a buscar sobre com fer-ho, i vaig trobar el món de els servidors privats de RuneScape, que eren una rèplica del joc en codi Java...",
+    "Knowledgeable and collaborator in most project phases: analysis, functional requirements, documentations, task organization, development, testing, version control, steps to production, integrations and customer training."
+        => "Coneixedor i col·laborador en la majoria de fases de projecte: anàlisi, requisits funcionals, documentacions, organització de tasques, desenvolupament, proves, control de versions, passos a producció, integracions i formació a client."
+
+    //Skills part
+
+    //Personal projects part
+
+    //Blogs part
+
+    //Contact me part
+
 ];
--- views/components/_download_button_submit.php
+++ views/components/_download_button_submit.php
@@ -1,5 +1,5 @@
 <div class="submit-btn">
-    <a href="/files/JOEL_FAURA_ES.pdf" target="_blank" class="main-btn"> <!-- TODO: Get the current web language to return the CV file with the same language as current (cv is like  JOEL_FAURA_LANG.pdf) -->
+    <a href="/files/JOEL_FAURA_<?= Yii::$app->language ?>.pdf" target="_blank" class="main-btn"> <!-- TODO: Get the current web language to return the CV file with the same language as current (cv is like  JOEL_FAURA_LANG.pdf) -->
         <span class="btn-text"><?= Yii::t('app','Download CV'); ?></span>
         <span class="btn-icon"><i class="fas fa-download"></i></span>
     </a>
